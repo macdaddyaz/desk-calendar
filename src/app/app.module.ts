5959df6943d3b2fdebeae97ff3b7d420891794aa
Eric McIntyre <emcintyre@idt911.com>
2016-09-29 15:51:55 -0700

    Removed the default 'app' component and replaced it with a 'calendar' component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +15,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [CalendarComponent]
 })
 export class AppModule { }
