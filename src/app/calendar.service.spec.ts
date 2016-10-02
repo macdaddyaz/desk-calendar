7727f25ec75b00401432449789a0276b39a51d11
Eric McIntyre <emcintyre@idt911.com>
2016-10-02 08:59:06 -0700

    Added very basic functionality to CalendarService
    Created unit tests for this functionality.

--- src/app/calendar.service.spec.ts
+++ src/app/calendar.service.spec.ts
@@ -1,7 +1,7 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
-import { CalendarService } from './calendar.service';
+import {TestBed, inject} from '@angular/core/testing';
+import {CalendarService} from './calendar.service';
 
 describe('Object: Calendar', () => {
   let service: CalendarService;
@@ -12,6 +12,34 @@ describe('Object: Calendar', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should initialize with the given month and year', () => {
+    service.init(2016, 8);
+
+    expect(service.getMonth()).toEqual(8);
+    expect(service.getYear()).toEqual(2016);
+    expect(service.getMonthName()).toEqual('September');
+
+    service.init(2014, 2);
+
+    expect(service.getMonth()).toEqual(2);
+    expect(service.getYear()).toEqual(2014);
+    expect(service.getMonthName()).toEqual('March');
+  });
+
+  it('should initialize with the current month and year', () => {
+    service.init();
+
+    let now = new Date();
+    expect(service.getMonth()).toEqual(now.getMonth());
+    expect(service.getYear()).toEqual(now.getFullYear());
+
+    // Only set year, not month
+    service.init(2014);
+    // Should default whole date
+    expect(service.getMonth()).toEqual(now.getMonth());
+    expect(service.getYear()).toEqual(now.getFullYear());
   });
 });
 
--- src/app/calendar.service.ts
+++ src/app/calendar.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import * as moment from 'moment';
 
 @Injectable()
 export class CalendarService {
 
-  constructor() { }
+  private year: number;
+  private month: number;
 
+  constructor() {
+  }
+
+  init(year?: number, month?: number) {
+    if (year && month) {
+      this.year = year;
+      this.month = month;
+    }
+    else {
+      let now = moment();
+      this.year = now.year();
+      this.month = now.month();
+    }
+  }
+
+  getYear(): number {
+    return this.year;
+  }
+
+  getMonth(): number {
+    return this.month;
+  }
+
+  getMonthName(): string {
+    return moment.months(this.month);
+  }
 }
