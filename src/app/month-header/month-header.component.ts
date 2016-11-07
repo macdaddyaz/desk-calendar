e8fabda4cbd6fca76acfb57ebd572b10f25f9eec
Eric McIntyre <emcintyre@idt911.com>
2016-11-07 16:36:54 -0700

    The `MonthHeaderComponent` updates as navigation occurs
    Implemented the `DoCheck` interface instead of `OnInit`. This allowed the component to detect that changes occurred and update itself from the latest `CalendarService` state.
    
    ref issue #5

--- src/app/month-header/month-header.component.ts
+++ src/app/month-header/month-header.component.ts
@@ -1,19 +1,19 @@
-import {Component, OnInit} from '@angular/core';
-import {CalendarService, YearAndMonth} from '../calendar.service';
+import {Component, DoCheck} from '@angular/core';
+import {CalendarService} from '../calendar.service';
 
 @Component({
   selector: 'cal-month-header',
   templateUrl: './month-header.component.html',
   styleUrls: ['./month-header.component.css']
 })
-export class MonthHeaderComponent implements OnInit {
+export class MonthHeaderComponent implements DoCheck {
   month: string = null;
   year: number = Number.MIN_SAFE_INTEGER;
 
   constructor(private calendarService: CalendarService) {
   }
 
-  ngOnInit() {
+  ngDoCheck(): void {
     this.updateFromCalendar();
   }
 
