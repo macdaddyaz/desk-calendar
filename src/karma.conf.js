d6073cb9557e90c034dbc0336d92fc36db610de3
Eric McIntyre <emcintyre@cyberscout.com>
2018-08-09 17:07:11 -0700

    Fix #35: Update TravisCI config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,8 +1,8 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// https://karma-runner.github.io/2.0/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
+  const settings = {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
@@ -27,5 +27,17 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false
-  });
+  };
+  if (process.env.TRAVIS === 'true') {
+    settings.autoWatch = false;
+    settings.singleRun = true;
+    settings.browsers = ['ChromeHeadlessNoSandbox']
+    settings.customLaunchers = {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    };
+  }
+  config.set(settings);
 };
