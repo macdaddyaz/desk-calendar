9c7b533a0449048f2c5258cb7c99fd2aec5e279a
Eric McIntyre <emcintyre@cyberscout.com>
2018-08-13 16:12:52 -0700

    Issue #38, #39 Integrated color theme
    Tweaked dev server launch to open Chrome and only bind to localhost.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,4 +3,13 @@ import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify, {
+  theme: {
+    primary: '#64B5F6',
+    secondary: '#E3F2FD',
+    accent: '#26A69A',
+    error: '#F44336',
+    warning: '#FF9800',
+    info: '#2196f3',
+    success: '#4caf50',
+  },
 });
