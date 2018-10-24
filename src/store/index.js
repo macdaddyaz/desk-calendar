8eb284a2ba9bce8d5371b5611e4250c751047593
Eric McIntyre <emcintyre@cyberscout.com>
2018-10-24 11:27:51 -0700

    Re-added Vuex store using re-implemented calendar functions

---
+++ src/store/index.js
@@ -0,0 +1,34 @@
+import {
+  currentMonth,
+  daysOfMonth,
+  goToMonth,
+  monthName,
+  nextMonth,
+  previousMonth,
+  weekdayNames,
+  year,
+} from '@/store/calendar';
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+const initialMonth = currentMonth();
+
+export default new Vuex.Store({
+  state: {
+    selectedMonth: initialMonth,
+  },
+  getters: {
+    previousMonth,
+    nextMonth,
+    monthName,
+    year,
+    weekdayNames,
+    daysOfMonth,
+  },
+  mutations: {
+    goToMonth,
+  },
+  actions: {},
+});
